a4913583f9903100c3498ac359f763fdec35316a
MrStandaert <dieter.standaert@kahiel.be>
2025-11-30 21:30:56 +0100

    aanpassing main

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 let teller = 0
 input.onPinPressed(TouchPin.P0, function () {
     basic.showIcon(IconNames.No)
+    basic.clearScreen()
     teller += 1
     basic.showNumber(teller)
 })
 input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
     teller = 0
 })
